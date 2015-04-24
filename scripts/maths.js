6872b5abce69533edca216baa65e9bbfd682683e
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-04-24 01:12:12 +0100

    Solve questions 1a and 1b

--- scripts/maths.js
+++ scripts/maths.js
@@ -1,58 +1,138 @@
-function analyzeWords(words) {
-  words = words.replace(/[^a-zA-Z ]/g, " ");
-  var arr = words.match(/\S+/g)
-    , wordCount = getCount(arr);
-  document.getElementById('textOuput').textContent = getTopTen(wordCount);
-}
+// The "mean" is the "average" you're used to,
+// where you add up all the numbers and then divide by the number of numbers.
+// The "median" is the "middle" value in the list of numbers. To find the median,
+// your numbers have to be listed in numerical order,
+// so you may have to rewrite your list first.
+// The "mode" is the value that occurs most often.
+// If no number is repeated, then there is no mode for the list.
 
-function getTopTen(wordList) {
-  var arr = []
-    , stop = -1;
-  if (wordList.length < 10) { stop = wordList.length; }
-  else { stop = 10; }
+/*
+** Text from 50 Shades of Grey, page 195.
+*/
+var mainText = "“Anastasia,” he warns, and I want to roll my eyes but quickly stop myself.  I stand facing the bed.  Sitting beside me, he gently pulls my sweatpants down again.  Up and down like whores’ drawers my subconscious remarks bitterly.  In my head, I tell her where to go.  Christian squirts baby oil into his hand and then rubs my behind with careful tenderness – from makeup remover to smoothing balm for a spanked ass, who would have thought it was such a versatile liquid. “I like my hands on you,” he murmurs, and I have to agree, me too. "
 
-  j = 1;
-  for (var i = 0; i < stop; i++) {
-    var key = Object.keys(wordList[i])[0]
-      , str = j + ". "
-            + '[' + wordList[i][key] + '] '
-            + key;
-    j++;
-    arr.push(str);
+document.getElementById('textInput').value = mainText;
+analyzeWords(mainText);
+
+var wordList = []
+  , frequencyTable = []
+  , modeWordLength = -1
+  , medianWordLength = -1
+  , meanWordLength = -1;
+
+  function OnInput (event) {
+      analyzeWords(event.target.value);
+  }
+  function OnPropChanged (event) {
+      if (event.propertyName.toLowerCase () == "value") {
+          analyzeWords(event.srcElement.value);
+      }
   }
 
-  return arr.join('\n');
+function analyzeWords(str) {
+  wordList = createWordList(str);
+
+  // Question 1(a)
+  frequencyTable = createFrequencyTable(wordList);
+  drawFrequencyTable(document.getElementById('frequencyTable'), frequencyTable);
+
+  // Question 1(b)
+  meanWordLength = getMean(wordList);
+  document.getElementById('mean').textContent = meanWordLength;
+
+  medianWordLength = getMedian(wordList);
+  document.getElementById('median').textContent = medianWordLength;
+
+  modeWordLength = getMode(frequencyTable);
+  document.getElementById('mode').textContent = modeWordLength;
 }
 
-function getCount(words) {
-  if(words == null || words.lenght < 1) { return []; }
-  var counter = {};
-  for (word of words) {
-    if (word in counter) { counter[word]++; }
-    else { counter[word] = 1; }
-  }
+function createWordList(words) {
+  words = words.replace(/[^a-zA-Z0-9 ]/g, " ");
 
-  var str = ''
+  return words.match(/\S+/g);
+}
+
+function createFrequencyTable(wordList) {
+  var obj = {}
     , arr = [];
 
-  for (var key in counter) {
-    if (counter.hasOwnProperty(key)) {
-      var obj = {};
-      obj[key] = counter[key];
-      arr.push(obj);
+  if (wordList == null || wordList == undefined) { return []; }
+
+  for (word of wordList) {
+    if (word.length in obj) { obj[word.length]++; }
+    else { obj[word.length] = 1; }
+  }
+
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      var o = {};
+      o[key] = obj[key];
+      arr.push(o);
     }
   }
 
-  arr.sort(compare);
+  arr.sort(sortObject);
   arr.reverse();
 
   return arr;
 }
 
-function compare(a,b) {
+function drawFrequencyTable(tbody, tableData) {
+
+  while (tbody.hasChildNodes()) {
+      tbody.removeChild(tbody.lastChild);
+  }
+
+  for (row of tableData) {
+    var tr = tbody.insertRow()
+      , td1 = tr.insertCell()
+      , td2 = tr.insertCell();
+
+    td1.appendChild(document.createTextNode(firstKeyOf(row)));
+    td2.appendChild(document.createTextNode(firstValueOf(row)));
+
+    tbody.appendChild(tr);
+  }
+
+  return tbody;
+}
+
+function getMean(wordList) {
+  var sum = 0;
+  if (wordList == null || wordList.length < 1)  { return -1; }
+  for (word of wordList) { sum += word.length; }
+  return roundD(sum / wordList.length, 3);
+}
+
+function getMedian(wordList) {
+  if (wordList == null ||  wordList.length < 1) { return -1; }
+  if (wordList.length == 1) { return wordList[0].length; }
+
+  var arr = [];
+  for (word of wordList) { arr.push(word.length); }
+  arr.sort(sortNumber);
+
+  var index = Math.round((arr.length - 1)/2);
+  if (arr.length % 1 == 1) { return arr[index] }
+  else { return (arr[index] + arr[index - 1]) / 2; }
+}
+
+function getMode(frequencyTable) {
+  if (frequencyTable == null || frequencyTable.length < 1) { return -1; }
+  return firstKeyOf(frequencyTable[0]);
+}
+
+// ---- Helper methods ----
+
+function sortObject(a,b) {
   if (firstValueOf(a) < firstValueOf(b)) { return -1; }
   if (firstValueOf(a) > firstValueOf(b)) { return 1; }
   return 0;
+}
+
+function sortNumber(a,b) {
+  return a - b;
 }
 
 function firstValueOf(obj) {
@@ -61,19 +141,12 @@ function firstValueOf(obj) {
   }
 }
 
-function OnClick() {
-  var textArea = document.getElementById('textInput');
-
-  // alert(textArea.value);
-
-  // analyzeWords(textArea.value);
+function firstKeyOf(obj) {
+  for (key in obj) {
+    return key;
+  }
 }
 
-function OnInput (event) {
-    analyzeWords(event.target.value);
-}
-function OnPropChanged (event) {
-    if (event.propertyName.toLowerCase () == "value") {
-        analyzeWords(event.srcElement.value);
-    }
+function roundD(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
